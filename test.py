7bd6c62d6d2cb4c563020a5da4fa6da117db5f09
RkAkDkHkA <deepanshusain75@gmail.com>
2026-09-05 15:36:57 +0530

    Add Streamlit and Snowflake imports to test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import os
+import streamlit as st
+import os
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
 st.write("Current working directory:", os.getcwd())
