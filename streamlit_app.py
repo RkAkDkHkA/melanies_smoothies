6596f64740c11957a11ad24ac68bbb70d8eb4738
RkAkDkHkA <deepanshusain75@gmail.com>
2026-09-06 12:16:33 +0530

    Display search values for selected fruit ingredients
    Updated the Streamlit app to display search values for selected ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),(col('Search_on')))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
-st.write(pd_df)
+
 
                                                                       
 
@@ -46,6 +46,8 @@ if ingredients_list:
 if ingredients_list:
 
     for fruit_chosen in ingredients_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen+' '+"Nutrition Information")
         url = "https://my.smoothiefroot.com/api/fruit/"
